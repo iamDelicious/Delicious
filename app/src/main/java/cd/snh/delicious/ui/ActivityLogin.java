c79b3a057737f050f2d67857fa3a83318ad76fae
Michelo <misamuna@gmail.com>
2018-01-17 13:23:01 +0200

    Update project with some Login Data purpose

---
+++ app/src/main/java/cd/snh/delicious/ui/ActivityLogin.java
@@ -0,0 +1,12 @@
+package cd.snh.delicious.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Michelo on 1/17/18.
+ */
+
+public class ActivityLogin extends AppCompatActivity{
+
+
+}
